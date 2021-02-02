81bb21784f0a26bd6b3de9bfdb523dd7062af00a
Patrick Breen <patrick.breen@quartzy.com>
2021-02-02 12:48:49 -0500

    Allow null values in custom casts

--- src/Casts/Money.php
+++ src/Casts/Money.php
@@ -4,25 +4,30 @@ declare(strict_types=1);
 
 namespace Tipoff\Support\Casts;
 
+use Brick\Money\Currency;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class Money implements CastsAttributes
 {
-    /** @var string */
+    /** @var Currency */
     private $currency;
 
     public function __construct(string $currency = 'USD')
     {
-        $this->currency = $currency;
+        $this->currency = Currency::of($currency);
     }
 
     public function get($model, string $key, $value, array $attributes)
     {
-        return \Brick\Money\Money::ofMinor($value, $this->currency);
+        return is_null($value) ? $value : \Brick\Money\Money::ofMinor($value, $this->currency);
     }
 
     public function set($model, string $key, $value, array $attributes)
     {
+        if (is_null($value)) {
+            return $value;
+        }
+
         if ($value instanceof \Brick\Money\Money) {
             return $value->getUnscaledAmount()->toInt();
         }
