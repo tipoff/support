5d9f9d75d1947ceec9bcbf6602e72ad4e01d0e19
Phuc Le <phuclh93@gmail.com>
2021-02-04 15:57:10 -0800

    Add all model references.

--- config/support.php
+++ config/support.php
@@ -2,4 +2,86 @@
 
 return [
 
+    'user' => \App\Models\User::class,
+
+    'slot' => \App\Models\Slot::class,
+
+    'booking' => \App\Models\Booking::class,
+
+    'schedule' => \App\Models\Schedule::class,
+
+    'schedule_eraser' => \App\Models\ScheduleEraser::class,
+
+    'signature' => \App\Models\Signature::class,
+
+    'recurring_schedule' => \App\Models\RecurringSchedule::class,
+
+    'game' => \App\Models\Game::class,
+
+    'location' => \App\Models\Location::class,
+
+    'market' => \App\Models\Market::class,
+
+    'block' => \App\Models\Block::class,
+
+    'cart' => \App\Models\Cart::class,
+
+    'cart_item' => \App\Models\CartItem::class,
+
+    'competitor' => \App\Models\Competitor::class,
+
+    'contact' => \App\Models\Contact::class,
+
+    'customer' => \App\Models\Customer::class,
+
+    'discount' => \App\Models\Discount::class,
+
+    'fee' => \App\Models\Fee::class,
+
+    'feedback' => \App\Models\Feedback::class,
+
+    'flex_day' => \App\Models\FlexDay::class,
+
+    'hold' => \App\Models\Hold::class,
+
+    'insight' => \App\Models\Insight::class,
+
+    'invoice' => \App\Models\Invoice::class,
+
+    'key' => \App\Models\Key::class,
+
+    'note' => \App\Models\Note::class,
+
+    'order' => \App\Models\Order::class,
+
+    'participant' => \App\Models\Participant::class,
+
+    'payment' => \App\Models\Payment::class,
+
+    'rate' => \App\Models\Rate::class,
+
+    'refund' => \App\Models\Refund::class,
+
+    'review' => \App\Models\Review::class,
+
+    'room' => \App\Models\Room::class,
+
+    'snapshot' => \App\Models\Snapshot::class,
+
+    'status' => \App\Models\Status::class,
+
+    'supervision' => \App\Models\Supervision::class,
+
+    'tax' => \App\Models\Tax::class,
+
+    'theme' => \App\Models\Theme::class,
+
+    'voucher' => \App\Models\Voucher::class,
+
+    'voucher_type' => \App\Models\VoucherType::class,
+
+    'image' => \DrewRoberts\Media\Models\Image::class,
+
+    'video' => \DrewRoberts\Media\Models\Video::class,
+
 ];
