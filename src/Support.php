7160f72c3b4b73a48c93c35eef6aed01d94e8c6e
Michael Slowik <slowik.michael@gmail.com>
2021-02-06 16:29:56 -0500

    resolve helper

--- src/Support.php
+++ src/Support.php
@@ -4,4 +4,13 @@ namespace Tipoff\Support;
 
 class Support
 {
+    /**
+     * Resolve a service from the container.
+     *
+     * @param  string  $name
+     * @return mixed
+     */
+    public static function resolve($name) {
+        return app($name);
+    }
 }
