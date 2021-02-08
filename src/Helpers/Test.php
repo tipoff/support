cd246ae2738870f48daf36e2211bd444bb9ac3e8
sl0wik <sl0wik@users.noreply.github.com>
2021-02-08 15:20:34 +0000

    Fix styling

--- src/Helpers/Test.php
+++ src/Helpers/Test.php
@@ -5,21 +5,21 @@ declare(strict_types=1);
 use Illuminate\Database\Eloquent\Model;
 
 if (! function_exists('randomOrCreate')) {
-	/**
-	 * Ger random model or create it using factory.
-	 *
-	 * @param  string|Model $classNameOrModel
-	 * @return Model
-	 */
+    /**
+     * Ger random model or create it using factory.
+     *
+     * @param  string|Model $classNameOrModel
+     * @return Model
+     */
     function randomOrCreate($classNameOrModel): Model
     {
-    	if (is_string($classNameOrModel)) {
-    		$className = $classNameOrModel;
-    	}
+        if (is_string($classNameOrModel)) {
+            $className = $classNameOrModel;
+        }
 
-    	if ($classNameOrModel instanceof Model) {
-    		$className = $classNameOrModel;
-    	}
+        if ($classNameOrModel instanceof Model) {
+            $className = $classNameOrModel;
+        }
 
         if ($className::count() > 0) {
             return $className::all()->random();
