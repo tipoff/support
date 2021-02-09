b8722f998943350ea03501e463e44a01b898eb2b
pdbreen <pdbreen@users.noreply.github.com>
2021-02-09 21:27:38 +0000

    Fix styling

--- src/Contracts/Models/CartInterface.php
+++ src/Contracts/Models/CartInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Support\Contracts\Models;
 
-use Brick\Money\Money;
-
 interface CartInterface extends BaseModelInterface
 {
     public function getLocation(): ?LocationInterface;
--- src/Contracts/Services/FeeService.php
+++ src/Contracts/Services/FeeService.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Support\Contracts\Services;
 
-use Tipoff\Support\Contracts\Models\CartItemInterface;
 use Tipoff\Support\Contracts\Models\FeeInterface;
 
 interface FeeService extends BaseService
