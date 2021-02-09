9b662bf958b6a8d10f76e1547a7d92a87d8050af
Christopher Pecoraro <chrispecoraro@gmail.com>
2021-02-08 22:23:24 -0500

    Add HasCreator and HasUpdater traits.

---
+++ src/Traits/HasCreator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Support\Traits;
+
+trait HasCreator
+{
+    protected static function bootHasCreator()
+    {
+        static::saving(function ($model) {
+            if (auth()->check()) {
+                $model->creator_id = auth()->id();
+            }
+        });
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator()
+    {
+        return $this->belongsTo(app('user'), 'creator_id');
+    }
+
+}
