4c54262976cbbc1f42bcbff720fe355a5c3da6a0
Patrick Breen <patrick.breen@quartzy.com>
2021-02-23 12:54:58 -0500

    Add DiscountableValue object and cast

---
+++ tests/Unit/Objects/DiscountableValueTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Support\Tests\Unit\Objects;
+
+use Tipoff\Support\Objects\DiscountableValue;
+use Tipoff\Support\Tests\TestCase;
+
+class DiscountableValueTest extends TestCase
+{
+    /** @test */
+    public function construct_with_value()
+    {
+        $value = new DiscountableValue(1000);
+
+        $this->assertEquals(1000, $value->getOriginalAmount());
+        $this->assertEquals(0, $value->getDiscounts());
+        $this->assertEquals(1000, $value->getDiscountedAmount());
+    }
+
+    /** @test */
+    public function add_discounts()
+    {
+        $value = (new DiscountableValue(1000))
+            ->addDiscounts(200);
+
+        $this->assertEquals(1000, $value->getOriginalAmount());
+        $this->assertEquals(200, $value->getDiscounts());
+        $this->assertEquals(800, $value->getDiscountedAmount());
+    }
+
+    /** @test */
+    public function add_multiple_discounts()
+    {
+        $value = (new DiscountableValue(1000))
+            ->addDiscounts(300)
+            ->addDiscounts(300)
+            ->addDiscounts(300);
+
+        $this->assertEquals(1000, $value->getOriginalAmount());
+        $this->assertEquals(900, $value->getDiscounts());
+        $this->assertEquals(100, $value->getDiscountedAmount());
+
+        $result = $value->addDiscounts(300);
+        $this->assertEquals(1000, $result->getOriginalAmount());
+        $this->assertEquals(1000, $result->getDiscounts());
+        $this->assertEquals(0, $result->getDiscountedAmount());
+
+        // Value should be unchanged
+        $this->assertEquals(1000, $value->getOriginalAmount());
+        $this->assertEquals(900, $value->getDiscounts());
+        $this->assertEquals(100, $value->getDiscountedAmount());
+    }
+
+    /** @test */
+    public function add_discountable_value()
+    {
+        $value1 = (new DiscountableValue(1000))
+            ->addDiscounts(300);
+
+        $value2 = (new DiscountableValue(500))
+            ->addDiscounts(100);
+
+        $result = $value1->add($value2);
+        $this->assertEquals(1500, $result->getOriginalAmount());
+        $this->assertEquals(400, $result->getDiscounts());
+        $this->assertEquals(1100, $result->getDiscountedAmount());
+
+        // Value 1 unchanged
+        $this->assertEquals(1000, $value1->getOriginalAmount());
+        $this->assertEquals(300, $value1->getDiscounts());
+        $this->assertEquals(700, $value1->getDiscountedAmount());
+
+        // Value 2 unchanged
+        $this->assertEquals(500, $value2->getOriginalAmount());
+        $this->assertEquals(100, $value2->getDiscounts());
+        $this->assertEquals(400, $value2->getDiscountedAmount());
+    }
+}
