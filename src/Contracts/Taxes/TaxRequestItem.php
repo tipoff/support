0e25ef29c3f6c729d9b13aaf2af8f050559c2ea9
devjk1 <devjk1@users.noreply.github.com>
2021-02-24 03:11:16 +0000

    Fix styling

--- src/Contracts/Taxes/TaxRequestItem.php
+++ src/Contracts/Taxes/TaxRequestItem.php
@@ -7,13 +7,18 @@ namespace Tipoff\Support\Contracts\Taxes;
 interface TaxRequestItem
 {
     public function getItemId(): string;
+
     public function getLocationId(): int;
+
     public function getTaxCode(): string;
+
     public function getAmount(): int;
 
     public function setTaxDescription(?string $taxDescription): self;
+
     public function getTaxDescription(): ?string;
 
     public function setTax(int $tax): self;
+
     public function getTax(): int;
 }
