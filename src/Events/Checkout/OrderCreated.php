7ecb964475aa25768e81726cfe8632e585178b33
Patrick Breen <patrick.breen@quartzy.com>
2021-02-25 12:34:53 -0500

    Add order events and generic PaymentInterface

---
+++ src/Events/Checkout/OrderCreated.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Support\Events\Checkout;
+
+use Tipoff\Support\Contracts\Checkout\CartInterface;
+use Tipoff\Support\Contracts\Checkout\OrderInterface;
+use Tipoff\Support\Events\BaseEvent;
+
+class OrderCreated extends BaseEvent
+{
+    public OrderInterface $order;
+
+    public function __construct(OrderInterface $order)
+    {
+        $this->order = $order;
+    }
+}
