a1de012d3c0bc83d416c2a1b863671bc3463ba8c
Patrick Breen <patrick.breen@quartzy.com>
2021-02-25 14:27:56 -0500

    Update typehints

--- src/Contracts/Checkout/CodedCartAdjustment.php
+++ src/Contracts/Checkout/CodedCartAdjustment.php
@@ -6,9 +6,15 @@ namespace Tipoff\Support\Contracts\Checkout;
 
 interface CodedCartAdjustment
 {
+    /**
+     * Locates an active adjustment by its code.  Null if nothing found.
+     *
+     * @param string $code
+     * @return static|null
+     */
     public static function findByCode(string $code);
 
-    public static function calculateAdjustments(CartInterface $cart);
+    public static function calculateAdjustments(CartInterface $cart): void;
 
     /**
      * @param CartInterface $cart
@@ -18,11 +24,17 @@ interface CodedCartAdjustment
 
     /**
      * Associates adjustment with cart
+     *
+     * @param CartInterface $cart
+     * @return static
      */
     public function applyToCart(CartInterface $cart);
 
     /**
      * Removes adjustment from cart
+     *
+     * @param CartInterface $cart
+     * @return static
      */
     public function removeFromCart(CartInterface $cart);
 }
