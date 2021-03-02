f8f725e44b24ab2afdded08ca6e2b4d686ed8f70
Patrick Breen <patrick.breen@quartzy.com>
2021-03-02 18:10:22 -0500

    Cleanup for merge

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,35 @@
 
 namespace Tipoff\Support\Tests;
 
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
+use Illuminate\Support\Facades\View as ViewFacade;
+use Illuminate\Support\Str;
+use Illuminate\Testing\TestView;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Tipoff\Support\SupportServiceProvider;
 
 class TestCase extends Orchestra
 {
+    use InteractsWithViews;
+
+    protected function blade(string $template, array $data = [])
+    {
+        $tempDirectory = sys_get_temp_dir();
+
+        if (! in_array($tempDirectory, ViewFacade::getFinder()->getPaths())) {
+            ViewFacade::addLocation(sys_get_temp_dir());
+        }
+
+        $tempFile = tempnam($tempDirectory, 'laravel-blade').'.blade.php';
+
+        // Fix for Github Actions ci/cd on windows - this strips any .tmp from the tempfile name generated
+        $tempFile = preg_replace('/\.tmp\.blade/', '.blade', $tempFile);
+
+        file_put_contents($tempFile, $template);
+
+        return new TestView(view(Str::before(basename($tempFile), '.blade.php'), $data));
+    }
+
     protected function getPackageProviders($app)
     {
         return [
