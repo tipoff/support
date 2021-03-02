3db3d1eae465310b3ed801be753cef9a8744e26d
Patrick Breen <patrick.breen@quartzy.com>
2021-03-02 18:03:19 -0500

    Attempt temp file fix for windows

--- tests/Unit/View/Components/MoneyTest.php
+++ tests/Unit/View/Components/MoneyTest.php
@@ -5,11 +5,32 @@ declare(strict_types=1);
 namespace Tipoff\Support\Tests\Unit\View\Components;
 
 use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
+use Illuminate\Support\Facades\View as ViewFacade;
+use Illuminate\Support\Str;
+use Illuminate\Testing\TestView;
 use Tipoff\Support\Tests\TestCase;
 
 class MoneyTest extends TestCase
 {
     use InteractsWithViews;
+
+    protected function blade(string $template, array $data = [])
+    {
+        $tempDirectory = sys_get_temp_dir();
+
+        if (! in_array($tempDirectory, ViewFacade::getFinder()->getPaths())) {
+            ViewFacade::addLocation(sys_get_temp_dir());
+        }
+
+        $tempFile = tempnam($tempDirectory, 'laravel-blade').'.blade.php';
+
+        // Attempt fix for ci/cd on windows
+        $tempFile = preg_replace('/\.tmp\.blade/', '.blade', $tempFile);
+
+        file_put_contents($tempFile, $template);
+
+        return new TestView(view(Str::before(basename($tempFile), '.blade.php'), $data));
+    }
 
     /** @test */
     public function formats_money()
