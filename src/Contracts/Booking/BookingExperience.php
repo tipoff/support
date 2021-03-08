24da6a48c155a901d8833cd9403dee819e760779
Michael Slowik <slowik.michael@gmail.com>
2021-03-08 11:25:49 -0500

    booking experience interface

---
+++ src/Contracts/Booking/BookingExperience.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Support\Contracts\Booking;
+
+interface BookingExperience
+{
+	/**
+	 * Get label used in lists.
+	 *
+	 * @return string
+	 */
+    public function getLabel(): string;
+
+    /**
+     * Get experience id.
+     *
+     * @return mixed
+     */
+    public function getId();
+
+	/**
+	 * Get experience type.
+	 *
+	 * @return string
+	 */
+    public function getType(): string;
+
+}
