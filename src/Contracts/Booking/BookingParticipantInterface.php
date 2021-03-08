fdf5abffe334c466db74b4ce7c772d3ac459da47
Michael Slowik <slowik.michael@gmail.com>
2021-03-08 10:37:47 -0500

    Booking participant interface

--- src/Contracts/Booking/BookingParticipantInterface.php
+++ src/Contracts/Booking/BookingParticipantInterface.php
@@ -5,7 +5,36 @@ declare(strict_types=1);
 namespace Tipoff\Support\Contracts\Booking;
 
 use Tipoff\Support\Contracts\Models\BaseModelInterface;
+use Tipoff\Support\Contracts\Models\UserInterface;
 
 interface BookingParticipantInterface extends BaseModelInterface
 {
+	/**
+	 * Get label used in lists.
+	 *
+	 * @return string
+	 */
+    public function getLabel(): string;
+
+	/**
+	 * Get email.
+	 *
+	 * @return string
+	 */
+    public function getEmail(): string;
+
+	/**
+	 * Check is participant account verified.
+	 *
+	 * @return bool
+	 */
+    public function isVerified(): bool;
+
+    /**
+     * Returns the user that owns the container
+     *
+     * @return UserInterface
+     */
+    public function getUser(): UserInterface;
+
 }
