3a091be2bca256e7679cb1c32c0f1f83d3412506
Michael Slowik <slowik.michael@gmail.com>
2021-03-08 11:19:46 -0500

    Booking rate interface

---
+++ src/Contracts/Booking/BookingRateInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Support\Contracts\Booking;
+
+use Tipoff\Support\Contracts\Scheduler\SlotInterface;
+
+interface BookingRateInterface extends SlotInterface
+{
+	/**
+	 * Get label used in lists.
+	 *
+	 * @return string
+	 */
+    public function getLabel(): string;
+
+	/**
+	 * Get slug used in urls.
+	 *
+	 * @return string
+	 */
+    public function getSlug(): string;
+
+    /**
+     * Get amount in cents.
+     *
+     * @return int
+     */
+    public function getAmount(): int;
+
+    /**
+     * Get number of participants for the rate.
+     *
+     * @return int|null
+     */
+    public function getParticipantsLimit(): int;
+}
