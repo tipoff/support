7547f2aecaf41f9f9c7e2c8159c7af218f3986d9
Michael Slowik <slowik.michael@gmail.com>
2021-03-08 10:27:21 -0500

    slot interface

---
+++ src/Contracts/Scheduler/SlotInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Support\Contracts\Scheduler;
+
+use Tipoff\Support\Contracts\Models\BaseModelInterface;
+
+interface SlotInterface extends BaseModelInterface
+{
+}
