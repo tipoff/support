e98937a915fd996b3e917be350e5a014e3c576e8
Michael Slowik <slowik.michael@gmail.com>
2021-03-09 17:25:30 -0500

    Booking interface

--- src/Contracts/Booking/BookingInterface.php
+++ src/Contracts/Booking/BookingInterface.php
@@ -4,8 +4,60 @@ declare(strict_types=1);
 
 namespace Tipoff\Support\Contracts\Booking;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Tipoff\Support\Contracts\Booking\BookingSlotInterface;
 use Tipoff\Support\Contracts\Models\BaseModelInterface;
 
 interface BookingInterface extends BaseModelInterface
 {
+    /**
+     * Get timezone.
+     *
+     * @return string
+     */
+    public function getTimezone(): string;
+
+    /**
+     * Get label used in lists.
+     *
+     * @return string
+     */
+    public function getLabel(): string;
+
+    /**
+     * Get booking description used in details.
+     *
+     * @return string
+     */
+    public function getDescription(): string;
+
+    /**
+     * Get booking date.
+     *
+     * @return Carbon
+     */
+    public function getDate(): Carbon;
+
+    /**
+     * Get slot.
+     *
+     * @return Slot|null
+     */
+    public function getSlot(): BookingSlotInterface;
+
+    /**
+     * Get amount in cents.
+     *
+     * @return int
+     */
+    public function getAmount(): int;
+
+    /**
+     * Get amount in cents.
+     *
+     * @return int
+     */
+    public function participants(): BelongsToMany;
+
 }
