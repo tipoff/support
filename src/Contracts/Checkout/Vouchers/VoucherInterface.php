75a879039cee9bcec31caa3a6ef7774ed47975e3
Patrick Breen <patrick.breen@quartzy.com>
2021-03-15 13:46:53 -0400

    Document refund voucher

--- src/Contracts/Checkout/Vouchers/VoucherInterface.php
+++ src/Contracts/Checkout/Vouchers/VoucherInterface.php
@@ -10,5 +10,8 @@ use Tipoff\Support\Contracts\Models\UserInterface;
 
 interface VoucherInterface extends BaseModelInterface, CodedCartAdjustment
 {
+    /**
+     * Create a new Refund voucher for the user and amount indicated.
+     */
     public static function createRefundVoucher(int $locationId, UserInterface $user, int $amount): self;
 }
