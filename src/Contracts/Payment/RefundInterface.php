db88f2badd3ce753bddf89532fea41c4a53f3d86
Patrick Breen <patrick.breen@quartzy.com>
2021-03-15 13:45:54 -0400

    Cleanup and document

--- src/Contracts/Payment/RefundInterface.php
+++ src/Contracts/Payment/RefundInterface.php
@@ -8,5 +8,14 @@ use Tipoff\Support\Contracts\Models\BaseModelInterface;
 
 interface RefundInterface extends BaseModelInterface
 {
+    /**
+     * Create and persist a new refund for the amount indicated.  The created refund is NOT issued
+     * until an explicit issue request occurs.
+     */
     public static function createRefund(PaymentInterface $payment, int $amount, string $method): self;
+
+    /**
+     * Issue a pending refund, completing the refund process.
+     */
+    public function issue(): self;
 }
